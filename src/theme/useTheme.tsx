7e04691de4c6ead784d74917fbc3fb558c09b501
Rasul <overlord.ove@mail.ru>
2023-01-24 11:15:21 +0300

    Организация стилей, добавление темы

---
+++ src/theme/useTheme.tsx
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+
+interface UseThemeResult {
+  theme: Theme;
+  onToggle: () => void;
+}
+
+export default function useTheme(): UseThemeResult {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const onToggle = () => {
+    const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
+    setTheme(newTheme);
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+  };
+
+  return {
+    theme,
+    onToggle,
+  };
+}
