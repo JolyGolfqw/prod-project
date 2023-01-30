30170bc002f336954d9b061d5d1d520ca73c57b0
Rasul <overlord.ove@mail.ru>
2023-01-30 21:42:40 +0300

    Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,15 +7,15 @@ import useTheme from "app/providers/ThemeProvider/lib/useTheme";
 import { MainPage } from "pages/MainPage";
 import { AboutPage } from "pages/AboutPage";
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 export default function App() {
   const { theme, onToggle } = useTheme();
   return (
     <div className={classNames("app", { hover: true }, [theme])}>
-      <button onClick={onToggle}>Toggle</button>
       <div>
-        <Link to="/">Main</Link>
-        <Link to="/about">About</Link>
+        <Navbar />
+      <button onClick={onToggle}>Toggle</button>
       </div>
       <AppRouter />
     </div>
