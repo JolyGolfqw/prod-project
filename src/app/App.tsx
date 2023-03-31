2df02c3566527901c83d3edae1d6806d8058ce66
Rasul <overlord.ove@mail.ru>
2023-03-31 17:08:11 +0300

    fixiki

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,15 @@
-import { Suspense } from 'react';
-import './styles/index.scss';
-import { classNames } from '../shared/lib/classNames/classNames';
-import useTheme from 'app/providers/ThemeProvider/lib/useTheme';
-import { AppRouter } from './providers/router';
-import { Navbar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from "react";
+import "./styles/index.scss";
+import { classNames } from "../shared/lib/classNames/classNames";
+import useTheme from "app/providers/ThemeProvider/lib/useTheme";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
 
 export default function App () {
   const { theme } = useTheme();
   return (
-    <div className={classNames('app', { hover: true }, [theme])}>
+    <div className={classNames("app", { hover: true }, [theme])}>
       <Suspense fallback="">
         <div>
           <Navbar />
